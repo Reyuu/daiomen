d3e457a3fb03b8b51802882dfe2449f5a9eb8e12
Reyuu <rey444xd3@@gmail.com>
2017-06-02 14:24:29 +0200

    dialogue test, kinda working

--- dialogue.py
+++ dialogue.py
@@ -1,57 +1,95 @@
 import pygame
 import collections
 import ezpygame
-
+import re
+import os
 Point = collections.namedtuple("Point", ["x", "y"])
 
-class Selection:
+def load_png(name):
+    """
+    Load image and return image object
+    :param directory: 
+    :param name: 
+    """
+    fullname = os.path.join(name)
+    #fullname = name
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha() is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error as message:
+        print('Cannot load image: %s' % fullname)
+        raise SystemExit(message)
+
+    return image, image.get_rect()
+
+class Speaker:
     def __init__(self):
-        """
-        Selection class for handling game input.
-        """
-        """
-        self.state = { "set1",
-                       "ready",
-                       "processing",
-                       "idle" }
-        """
-        self.state = "idle"
-        self.pos1 = (-1, -1)
-        self.pos2 = (-1, -1)
+        self.image = ""
+        self.current_image = pygame.Surface((1, 1))
+        self.name = ""
+        self.visibility = True
 
-    def add_pos(self, pos):
-        if self.state == "idle":
-            self.pos1 = pos
-            self.state = "set1"
-            return None
-        if self.state == "set1":
-            self.pos2 = pos
-            self.state = "ready"
-            return None
+    def process(self):
+        print(self.image)
+        self.current_image = load_png(self.image)[0]
 
-    def check_ready(self):
-        if self.state == "ready":
-            self.state = "processing"
-            return True
-        return False
+    def handle_set(self, n, m):
+        print(n, m)
+        n = n.strip("\"")
+        m = m.strip("\"")
+        if n == "image":
+            self.image = m
+            self.process()
+        if n == "name":
+            self.name = m
+        if n == "visibility":
+            bl = {"false": False, "true": True}
+            self.visibility = bool(bl[m.lower()])
 
-    def after_process(self):
-        if self.state == "processing":
-            self.state = "idle"
-            self.pos1 = (-1, -1)
-            self.pos2 = (-1, -1)
 
-class Node:
-    def __init__(self, name, x, y):
-        self.name = name
-        self.text = "Example"
-        self.connected_to = []
-        self.pos = Point(x=x, y=y)
-        self.width = 100
-        self.height = 100
+class Scripting:
+    def __init__(self):
+        self.keywords = {}
+        self.constants = {"Speaker": Speaker}
+        self.variables = {"current_speaker": None, "current_line": "", "name1": Speaker(), "name2": Speaker()}
+        self.paused = False
 
-    def get_rect(self):
-        return pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
+    def parse_instruction(self, instruction):
+        print(instruction)
+        words = instruction.split(" ")
+        if words[0] == "create":
+            self.variables.update({words[2]: self.constants[words[1]]()})
+        if words[0] == "set":
+            try:
+                var = words[1].split(".")[0]
+                print(var)
+                if isinstance(self.variables[var], self.constants["Speaker"]):
+                    self.variables[var].handle_set(words[1].split(".")[1], words[2])
+            except IndexError:
+                pass
+
+    def parse_line(self, line):
+        if not(self.paused):
+            if line[0] == "#":
+                return "##comment"
+            if line[:2] == "<<":
+                self.parse_instruction(line[2:-3])
+                return "##instruction"
+            if line[0] == "[":
+                self.variables.update({"current_speaker": line[1:-2]})
+                return "##speaker_set"
+            if line == "\n":
+                self.paused = True
+                return "##paused"
+            else:
+                self.variables["current_line"] = line[:-1]
+                return line[:-1]
+        else:
+            return "##paused"
+
 
 def isPointInsideRect(x, y, rect):
     """
@@ -82,45 +120,67 @@ def doRectsOverlap(rect1, rect2):
             return True
     return False
 
+"""
+a = Scripting()
+with open("data/script/dialogue/001_RinaConsta_TEST.dia") as f:
+   content = f.readlines()
+#content = [x.strip() for x in content]
+content = collections.deque(content)
+initial = True
+item = ""
+while 1:
+    if initial:
+        item = content.popleft()
+        initial = False
+    if not(a.parse_line(item) == "##paused"):
+        item = content.popleft()
+    else:
+        pass
+"""
 
-
-
-
-class Editor(ezpygame.Scene):
+class Game(ezpygame.Scene):
     def __init__(self):
         super().__init__()
         pygame.font.init()
-        self.font = pygame.font.SysFont("Arial", 16)
-        self.counter = 0
-        self.node_collection = []
+        self.font = pygame.font.Font("data/foo.ttf", 32)
+        self.scripting = Scripting()
+        self.dialogue = collections.deque()
+        with open("data/script/dialogue/001_RinaConsta_TEST.dia") as f:
+            self.dialogue = collections.deque(f.readlines())
+        self.script_line = self.dialogue.popleft()
 
-    def check_collisions(self, x, y):
-        for i in range(len(self.node_collection)):
-            if isPointInsideRect(x, y, self.node_collection[i].get_rect()):
-                return i
-        return False
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            pos = Point(x=pos[0], y=pos[1])
-            if event.button == 1:
-                col = self.check_collisions(pos.x, pos.y)
-                if type(col) == int:
-                    self.node_collection[col].pos = pos
-                else:
-                    self.node_collection += [Node("Node%s" % self.counter, pos.x, pos.y)]
-                    self.counter += 1
 
     def draw(self, screen):
-        screen.fill((255, 255, 255))
-        for item in self.node_collection:
-            pygame.draw.rect(screen, (225, 225, 225), pygame.Rect(item.pos.x, item.pos.y, item.width, item.height))
-            pygame.draw.rect(screen, (235, 235, 235), pygame.Rect(item.pos.x, item.pos.y, item.width, 20))
-            name = self.font.render(item.name, True, (210, 210, 210))
-            text = self.font.render(item.text, True, (210, 210, 210))
-            screen.blit(name, item.pos)
-            screen.blit(text, (item.pos.x, item.pos.y+20))
+        screen.fill((100, 100, 100))
+        if self.scripting.paused:
+            cur_speaker = self.scripting.variables["current_speaker"]
+            line = self.font.render(self.scripting.variables["current_line"], True, (255, 255, 255))
+            print(self.scripting.variables[cur_speaker].current_image)
+            screen.blit(self.scripting.variables[cur_speaker].current_image, (0,0))
+            screen.blit(line, (50, self.application.resolution[1]-250))
+        pass
+
+    def update(self, dt):
+        result = self.scripting.parse_line(self.script_line)
+        if result == "##paused":
+            pass
+        else:
+            self.script_line = self.dialogue.popleft()
+
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("Trying to upause")
+                self.scripting.paused = False
+                self.script_line = self.dialogue.popleft()
+        pass
+
+    def on_enter(self, previous_scene=None):
+        pass
+
+    def on_exit(self, next_scene=None):
+        pass
+
 
 
 
@@ -130,4 +190,4 @@ app = ezpygame.Application(
     update_rate=60
 )
 
-app.run(Editor())
+app.run(Game())
